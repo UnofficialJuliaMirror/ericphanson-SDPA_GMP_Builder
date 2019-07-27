b5fb32ca618b5493d288923ddfcc47359d6a2fba
Eric Hanson <5846501+ericphanson@users.noreply.github.com>
2019-07-27 18:03:18 +0100

    Try building GMP too

--- build_tarballs.jl
+++ build_tarballs.jl
@@ -9,38 +9,52 @@ version = v"7.1.3"
 sources = [
     "https://sourceforge.net/projects/sdpa/files/sdpa-gmp/sdpa-gmp-7.1.3.src.20150320.tar.gz" =>
     "65591cfba18afe710508023cd0c4a9d36ca8c56a7dd312d1cf4fc962c7b90df4",
+    "https://gmplib.org/download/gmp/gmp-6.1.2.tar.bz2" =>
+    "5275bb04f4863a13516b2f39392ac5e272f5e1bb8057b18aec1c9b79d73d8fb2",
     "./bundled"
 ]
 
 # Bash recipe for building across all platforms
 script = raw"""
+
+# Build GMP
+
+cd $WORKSPACE/srcdir/gmp-*
+# Patch `configure` to include `$LDFLAGS` in its tests.  This is necessary on FreeBSD.
+atomic_patch -p1 ${WORKSPACE}/srcdir/patches/configure.patch
+# Include Julia-carried patches
+atomic_patch -p1 ${WORKSPACE}/srcdir/patches/gmp_alloc_overflow_func.patch
+atomic_patch -p1 ${WORKSPACE}/srcdir/patches/gmp-exception.patch
+flags=(--enable-cxx --disable-shared --enable-static)
+# On x86_64 architectures, build fat binary
+if [[ ${proc_family} == intel ]]; then
+    flags+=(--enable-fat)
+fi
+./configure --prefix=$prefix --host=$target ${flags[@]}
+make -j6
+make install
+
+
+# Build SDPA-GMP
+
 cd $WORKSPACE/srcdir/sdpa-gmp-7.1.3/
-
 update_configure_scripts
-
-for path in ${LD_LIBRARY_PATH//:/ }; do
-    for file in $(ls $path/*.la); do
-        echo "$file"
-        baddir=$(sed -n "s|libdir=||p" $file)
-        sed -i~ -e "s|$baddir|'$path'|g" $file
-    done
-done
 
 if [ $target = "x86_64-apple-darwin14" ]; then
   # seems static linking requires apple's ar
   export AR=/opt/x86_64-apple-darwin14/bin/x86_64-apple-darwin14-ar
 fi
-
-patch -p1 < $WORKSPACE/srcdir/patches/lt_init_gmp.diff
-mv configure.in configure.ac
-autoreconf -i
+# patch -p1 < $WORKSPACE/srcdir/patches/shared_gmp.diff
+#patch -p1 < $WORKSPACE/srcdir/patches/lt_init_gmp.diff
+#mv configure.in configure.ac
+#autoreconf -i
 
 CXXFLAGS="-I$prefix/include"; export CXXFLAGS
 CPPFLAGS="-I$prefix/include"; export CPPFLAGS
 CFLAGS="-I$prefix/include"; export CFLAGS
 LDFLAGS="-L$prefix/lib"; export LDFLAGS
 
-./configure --prefix=$prefix --host=$target --with-gmp-includedir=$prefix/include --with-gmp-libdir=$prefix/lib --disable-shared
+./configure --prefix=$prefix --host=$target lt_cv_deplibs_check_method=pass_all  --disable-shared --enable-static
 
 make -j6
 
